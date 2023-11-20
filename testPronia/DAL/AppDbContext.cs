b874cb8f3452ba56b228044c2d45788fc4e2f64e
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-11-20 23:46:30 +0400

    ProductTags and Tags tables created

--- testPronia/DAL/AppDbContext.cs
+++ testPronia/DAL/AppDbContext.cs
@@ -14,6 +14,10 @@ namespace testPronia.DAL
 
         public DbSet<Slide> Slides { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+
 
 
 
--- testPronia/Migrations/20231120194459_ProductTagsAndTagsTablesCreated.Designer.cs
+++ testPronia/Migrations/20231120194459_ProductTagsAndTagsTablesCreated.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using testPronia.DAL;
 
@@ -11,9 +12,11 @@ using testPronia.DAL;
 namespace testPronia.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231120194459_ProductTagsAndTagsTablesCreated")]
+    partial class ProductTagsAndTagsTablesCreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
