d86bda7cc9b8f2190a356e661ea42dde706b3c32
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-11-22 01:52:00 +0400

    Category table created

---
+++ testPronia/Areas/ProniaAdmin/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testPronia.Areas.ProniaAdmin.Controllers
+{
+    [Area("ProniaAdmin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
