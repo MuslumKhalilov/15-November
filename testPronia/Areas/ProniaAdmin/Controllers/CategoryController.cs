41f4a098618972f2fb7de86b549a5803089fd94e
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-11-22 04:26:33 +0400

    Required command and Max Lenght added to Category class. Checked if Category exists in database or not

--- testPronia/Areas/ProniaAdmin/Controllers/CategoryController.cs
+++ testPronia/Areas/ProniaAdmin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using testPronia.DAL;
 using testPronia.Models;
@@ -22,8 +23,28 @@ namespace testPronia.Areas.ProniaAdmin.Controllers
         }
         public IActionResult Create()
         {
-
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+				return View();
+			}
+            bool result = _context.Category.Any(c => c.Name.ToLower().Trim() == category.Name.ToLower().Trim());
+            if (result)
+            {
+                ModelState.AddModelError("Name","Category already exists");
+                return View();
+            }
+
+            await _context.Category.AddAsync(category);
+            await _context.SaveChangesAsync();
+            
+            return RedirectToAction("Index");
+            
         }
     }
 }
--- testPronia/Models/Category.cs
+++ testPronia/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace testPronia.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testPronia.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(25)]
         public string Name { get; set; }
         public List<Product>? Products { get; set; }
     }
