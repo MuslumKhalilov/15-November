36ea6655af8a53ab3c74e70e917a443a00992831
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-11-27 00:33:17 +0400

    CreateProductVM ViewModel created. Create Page created for Product table.

--- testPronia/Areas/ProniaAdmin/Controllers/ProductController.cs
+++ testPronia/Areas/ProniaAdmin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using testPronia.Areas.ViewModels;
 using testPronia.DAL;
 using testPronia.Models;
 
@@ -21,5 +22,38 @@ namespace testPronia.Areas.ProniaAdmin.Controllers
 
             return View(products);
         }
-    }
+
+
+		public async Task<IActionResult> Create()
+		{
+			ViewBag.Categories= await _context.Category.ToListAsync();
+
+			return View();
+		}
+        [HttpPost]
+		public async Task<IActionResult> Create(CreateProductVM createProductVM)
+		{
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            bool result = await _context.Category.AnyAsync(c => c.Id == createProductVM.CategoryId);
+            if (!result)
+            {
+                ModelState.AddModelError("CategoryId","Category doesn't exists");
+            }
+            Product product = new Product
+            {
+                Name = createProductVM.Name,
+                CategoryId = createProductVM.CategoryId,
+                Price = createProductVM.Price,
+                Description = createProductVM.Description,
+                SKU = createProductVM.SKU
+            };
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+			return View(nameof(Index));
+		}
+	}
 }
