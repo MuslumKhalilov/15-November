2f041728d1b937cbe0a561693bc0f10a926a2b12
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-11-26 23:25:27 +0400

    Product Table created. Datas of Product placed to Product Table

---
+++ testPronia/Areas/ProniaAdmin/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testPronia.DAL;
+using testPronia.Models;
+
+namespace testPronia.Areas.ProniaAdmin.Controllers
+{
+    [Area("ProniaAdmin")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+             List<Product> products= await _context.Products.Include(p=> p.Category).Include(p=> p.ProductImages.Where(pi=>pi.IsPrimary==true)) .ToListAsync();
+
+            return View(products);
+        }
+    }
+}
