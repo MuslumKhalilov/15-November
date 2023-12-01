c40bc7c53e312436f570f67cab53ccb2a13bb4ce
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-12-01 05:04:00 +0400

    Datas placed to footer with LayoutService

--- testPronia/Program.cs
+++ testPronia/Program.cs
@@ -6,10 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer("server=DESKTOP-CCR3N2B;database=Pronia;trusted_connection=true;integrated security=true;encrypt=false;"));
-
+builder.Services.AddScoped<LayoutService>();
 var app = builder.Build();
 
-builder.Services.AddScoped<LayoutService>();
+
 app.UseStaticFiles();
 app.UseRouting();
 
