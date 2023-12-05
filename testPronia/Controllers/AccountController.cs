2c5ccd958267d35468bcf61d6679ec7cb097e77d
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-12-05 05:38:39 +0400

    Task Updated

--- testPronia/Controllers/AccountController.cs
+++ testPronia/Controllers/AccountController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using testPronia.Models;
 using testPronia.ModelViews;
 
 namespace testPronia.Controllers
 {
 	public class AccountController : Controller
 	{
-		public IActionResult Register()
+		private readonly UserManager<AppUser> _userManagaer;
+		private readonly SignInManager<AppUser> _signInManager;
+
+		public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
+        {
+				_userManagaer=userManager;
+			_signInManager= signInManager;
+        }
+        public IActionResult Register()
 		{
 			
 			return View();
@@ -17,7 +27,25 @@ namespace testPronia.Controllers
 			{
 				return View();
 			}
-			return Json(registerVM);
+			AppUser user = new AppUser()
+			{
+				Name = registerVM.Name,
+				Surname = registerVM.Surname,
+				UserName = registerVM.Username,
+				Email = registerVM.Email
+			};
+
+			IdentityResult result= await _userManagaer.CreateAsync(user,registerVM.Password);
+			if (!result.Succeeded)
+			{
+				foreach (IdentityError error in result.Errors)
+				{
+					ModelState.AddModelError(String.Empty,error.Description);
+				}
+				return View();
+			}
+			await _signInManager.SignInAsync(user,false);
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
